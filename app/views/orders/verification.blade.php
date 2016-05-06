c34a1674675a2c21a9d0a99969787ab5045de709
Alan Gibson <ajw.gibson@gmail.com>
2016-05-06 18:29:12 +0100

    Include contact email address in the verification view

--- app/views/orders/verification.blade.php
+++ app/views/orders/verification.blade.php
@@ -7,8 +7,11 @@
 
 <p class="lead">
     Before we continue we need to verify the email address you provided
-    with your contact details. You should have received an email with a code.
-    Please enter the code below to continue with your order.
+    with your contact details. You should have received an email with a code,
+    please enter that code below to continue with your order. If you haven't 
+    received an email within a few minutes please check your spam folder.
+    Failing that, please contact us at
+    <a href="mailto:kids@causewaycoastvineyard.com">kids@causewaycoastvineyard.com</a>.
 </p>
 
 {{ Form::open(array('route' => array('order.verification.do', $transaction_id))) }}
