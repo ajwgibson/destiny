ce85817e256152df0875b34d401260221e44cb43
Alan Gibson <ajw.gibson@gmail.com>
2016-05-23 22:32:13 +0100

    Changes to the order confirmation email

--- app/views/emails/order/confirmation.blade.php
+++ app/views/emails/order/confirmation.blade.php
@@ -18,79 +18,89 @@
 
     <table border="0" cellspacing="3" cellpadding="2" style="margin-left: 20px;">
 
-        <tr valign="top">
-            <td nowrap>Order reference</td>
-            <td>{{ $order->transaction_id }}</td>
-        </tr>
+        <tr><td colspan="3"></td></tr>
 
         <tr valign="top">
-            <td nowrap>Contact details</td>
-            <td>
+            <td nowrap><b><small>Order reference</small></b></td>
+            <td colspan="2">{{ $order->transaction_id }}</td>
+        </tr>
+
+        <tr><td colspan="3"></td></tr>
+
+        <tr valign="top">
+            <td nowrap><b><small>Contact details</small></b></td>
+            <td colspan="2">
                 {{{ $order->name() }}}<br/>
                 {{{ $order->email }}}<br/>
                 {{{ $order->phone }}}
             </td>
         </tr>
 
-        <tr valign="top">
-            <td nowrap>Children</td>
-            <td>
-                <table border="0" cellspacing="2" cellpadding="2">
-                @foreach ($order->children as $child)
-                    <tr>
-                        <td>Name</td>               
-                        <td>{{ $child->name() }}</td>
-                    </tr>
-                    <tr>
-                        <td>Date of birth</td>      
-                        <td>{{ $child->date_of_birth->format('jS F Y') }}</td>
-                    </tr>
-                    <tr>
-                        <td>Age on August 3rd</td>  
-                        <td>{{ $child->age() }}</td>
-                    </tr>
-                    <tr>
-                        <td>T-shirt size</td>       
-                        <td>{{ $child->tshirt }}</td>
-                    </tr>
-                    <tr>
-                        <td>Dancing</td>            
-                        <td>{{ $child->dancing ? 'Yes' : 'No' }}</td>
-                    </tr>
-                    <tr>
-                        <td>Sleepover</td>          
-                        <td>{{ $child->sleepover ? 'Yes' : 'No' }}</td>
-                    </tr>
-                @endforeach
-                </table>
-            </td>
-        </tr>
+        <tr><td colspan="3"></td></tr>
 
         <tr valign="top">
-            <td nowrap>Permissions</td>
-            <td>
+            <td nowrap><b><small>Children</small></b></td>
+            <td colspan="2">({{ $order->children->count() }})</td>
+        </tr>
+        @foreach ($order->children as $child)
+        <tr>
+            <td></td>
+            <td colspan="2"><b>{{ $child->name() }}</b></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right"><small>Date of birth</small></td>      
+            <td>{{ $child->date_of_birth->format('jS F Y') }}</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right"><small>Age on August 3rd</small></td>  
+            <td>{{ $child->age() }}</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right"><small>T-shirt size</small></td>       
+            <td>{{ $child->tshirt }}</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right"><small>Dancing</small></td>            
+            <td>{{ $child->dancing ? 'Yes' : 'No' }}</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td align="right"><small>Sleepover</small></td>          
+            <td>{{ $child->sleepover ? 'Yes' : 'No' }}</td>
+        </tr>
+        @endforeach
+
+        <tr><td colspan="3"></td></tr>
+
+        <tr valign="top">
+            <td nowrap><b><small>Permissions</small></b></td>
+            <td colspan="2">
                 Photographs: {{ $order->photos_permitted ? 'Yes' : 'No' }}<br/>
                 Outings: {{ $order->outings_permitted ? 'Yes' : 'No' }}
             </td>
         </tr>
 
+        <tr><td colspan="3"></td></tr>
+
         <tr valign="top">
-            <td nowrap>Price</td>
-            <td>
-                @if ($order->cost() != $order->total())
-                <table border="0" cellspacing="2" cellpadding="2">
-                    <tr><td>Cost</td>     <td>£{{ money_format('%(#3i', $order->cost()) }}</td></tr>
-                    @if ($order->discount() > 0)
-                    <tr><td>Discount</td> <td>£{{ money_format('%(#3i', $order->discount()) }}</td></tr>
-                    @endif
-                    @if ($order->amount_extra > 0)
-                    <tr><td>Extra</td>    <td>£{{ money_format('%(#3i', $order->amount_extra) }}</td></tr>
-                    @endif
-                    <tr><td>Total</td>    <td>£{{ money_format('%(#3i', $order->total()) }}</td></tr>
-                </table>
-                @else
-                    £{{ money_format('%i', $order->total()) }}
+            <td nowrap><b><small>Price</small></b></td>
+            <td colspan="2">
+            @if ($order->cost() != $order->total())
+                Cost: &pound;{{ money_format('%(#3i', $order->cost()) }}<br/>
+                @if ($order->discount() > 0)
+                Discount: &pound;{{ money_format('%(#3i', $order->discount()) }}<br/>
                 @endif
+                @if ($order->amount_extra > 0)
+                Extra: &pound;{{ money_format('%(#3i', $order->amount_extra) }}<br/>
+                @endif
+                Total: &pound;{{ money_format('%(#3i', $order->total()) }}<br/>
+            @else
+                &pound;{{ money_format('%i', $order->total()) }}
+            @endif
             </td>
         </tr>
 
