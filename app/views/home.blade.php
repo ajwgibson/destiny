cd51863b2fc324eb2f6129ca7987903eafca61de
Alan Gibson <ajw.gibson@gmail.com>
2016-06-07 21:27:20 +0100

    Some tweaks to homepage blurb

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,18 +3,31 @@
 @section('content')
 
 <div class="jumbotron">
-    <h1>Welcome</h1>
+
+    <h1>The Kids Awaken</h1>
+
     <p>
-        To three days full of adventure for primary aged kids, filled with loads of fun,
-        where we’ll be unpacking loads of excitement through activities, games, and more! 
+        Each morning, 10am to 1pm will be packed with awesome games, activities, music, 
+        competitions, prizes and much more! Each child will receive a Destiny Island: Kids Awaken T-Shirt 
+        as part of their involvement.
     </p>
+ 
     <p>
-        And this year we're also running an overnight event for kids who are eleven or older!
+        We have an extra for those aged 10 and over; code-named “Destiny HighLand”. This is an 
+        opportunity to stay overnight on site from Thursday morning to Friday lunchtime. We will go 
+        on ‘mission’ to give the love of Jesus away in the afternoon in our town, then we will 
+        back for food and a sleep over in the church building. All meals will be catered for (£6 extra).
     </p>
-    <p>
-        Destiny Island is designed to help kids understand God’s love for them and show 
-        them how to give it away. Come join us in this wild, crazy, adventure called Destiny Island!
+        
+    <p> 
+        We are closing the week with a Family Fun Night &amp; BBQ from 6.30pm at CCV on Friday 5th. 
+        All families from Destiny Island are invited to this event.
     </p>
+        
+    <p> 
+        Can't wait to see you!
+    </p>
+
     <p>
         @if (Session::has('transaction_id'))
         
@@ -42,48 +55,59 @@
     </p>
 </div>
 
-<div class="row">
+<div class="row bottom-40">
     <div class="col-md-12">
-        <h3>Pricing details</h3>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default text-center pricing">
-            <div class="panel-heading">
-                <h2>Standard</h2>
-                <p>£10</p>
+        <div class="panel panel-info">
+            <div class="panel-heading text-center">
+                <h2>Pricing details</h2>
             </div>
             <div class="panel-body">
-                <p>
-                    <i>Entry to morning activities including Nerf challenge, Score football, Cooking, Flashdance.</i>
-                </p>
-            </div>  
-            <ul class="list-group">
-                <li class="list-group-item">Wed - Fri</li>
-                <li class="list-group-item">10:00am - 12:30pm</li>
-                <li class="list-group-item">T-Shirt included</li>
-            </ul>
-        </div>
-    </div>
-    <div class="col-md-6">
-        <div class="panel panel-default text-center pricing">
-            <div class="panel-heading">
-                <h2>Sleepover</h2>
-                <p>£6</p>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="panel panel-info text-center pricing">
+                            <div class="panel-heading">
+                                <h3>Conference pass</h3>
+                                <p>£10</p>
+                            </div>
+                            <div class="panel-body">
+                                <p>
+                                    <i>
+                                        Entry to our daily sessions packed with awesome games, activities, music, 
+                                        competitions, prizes and more.
+                                    </i>
+                                </p>
+                            </div>  
+                            <ul class="list-group">
+                                <li class="list-group-item">Wed - Fri</li>
+                                <li class="list-group-item">10am - 1pm</li>
+                                <li class="list-group-item">T-Shirt included</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="panel panel-info text-center pricing">
+                            <div class="panel-heading">
+                                <h3>Destiny High-Land</h3>
+                                <p>£6</p>
+                            </div>
+                            <div class="panel-body">
+                                <p>
+                                    <i>
+                                        Thursday afternoon 'mission' in town followed by
+                                        dinner and an overnight stay in the church.<br>
+                                        (for children aged 10 and older)
+                                    </i>
+                                </p>
+                            </div>  
+                            <ul class="list-group">
+                                <li class="list-group-item">Thu - Fri</li>
+                                <li class="list-group-item">10am Thursday - 10am Friday</li>
+                                <li class="list-group-item">Dinner included</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="panel-body">
-                <p>
-                    <i>An overnight stay for children aged 11 or older.</i>
-                    <br><br>
-                </p>
-            </div>  
-            <ul class="list-group">
-                <li class="list-group-item">Thu</li>
-                <li class="list-group-item">12:30pm - 10:00am</li>
-                <li class="list-group-item">Pizza included</li>
-            </ul>
         </div>
     </div>
 </div>
@@ -97,8 +121,10 @@
 <div class="panel">
     <div class="panel-body">
         <address>
+            <span class="h3">Destiny Island 2016</span><br>
             <span class="h4">Wed Aug 3rd - Fri Aug 5th</span><br>
-            <span class="h5">10:00am - 12:30pm</span>
+            <span class="h5">10am - 1pm</span><br>
+            <span class="h5">Ages 5 - 11</span>
         </address>
         <address>
             <strong>Address</strong><br>
